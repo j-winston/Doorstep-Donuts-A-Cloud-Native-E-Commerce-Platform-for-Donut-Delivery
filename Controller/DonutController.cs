405a03859bebfa9ee263b8cb7518953f719f6527
James Winston <kenjameswinston@gmail.com>
2024-11-07 13:18:24 -0500

    feat(controller): implement DonutController class

---
+++ Controller/DonutController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce.Models;
+
+public class DonutController : Controller
+{
+    private readonly IDonutRepository _donutRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public DonutController(IDonutRepository donutRepository, ICategoryRepository categoryRepository)
+    {
+        _donutRepository = donutRepository;
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult List()
+    {
+        return View(_donutRepository.AllDonuts);
+    }
+
+
+
+}
