5a4cb398fbacb501f9c81417a2ca704ff06300c0
James Winston <kenjameswinston@gmail.com>
2024-11-07 13:44:53 -0500

    feat(view): implement view template

--- Controller/DonutController.cs
+++ Controller/DonutController.cs
@@ -15,6 +15,8 @@ public class DonutController : Controller
 
     public IActionResult List()
     {
+        ViewBag.CurrentCategory = "Cake Donuts";
+
         return View(_donutRepository.AllDonuts);
     }
 
