6d7af10039e88b740998e10adaf4ba1c19e12938
James Winston <kenjameswinston@gmail.com>
2024-12-09 13:37:28 -0500

    cicd(docker): update docker files on dockerhub

--- Program.cs
+++ Program.cs
@@ -2,11 +2,10 @@ using e_commerce.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSession();
 
 //builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 //builder.Services.AddScoped<IDonutRepository, MockDonutRepository>();
-
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IDonutRepository, DonutRepository>();
 
@@ -24,7 +23,6 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Middleware pipeline
-// Static files 
 app.UseStaticFiles();
 
 // Exception display 
@@ -36,6 +34,8 @@ if (app.Environment.IsDevelopment())
 // Default convention based routing middleware
 app.MapDefaultControllerRoute();
 
+// Sessions 
+app.UseSession();
 
 // Seed data
 DbInitializer.Seed(app);
